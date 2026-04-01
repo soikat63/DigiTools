dfca054f47918251a9eed93894d95c77f1065136
soikat63 <soikat6388@gmail.com>
2026-04-01 10:01:56 +0600

    fixed the minor issue

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GradientButton from "./Ui/Button/GradientButton";
 import BannerImage from "../assets/Banner.png";
-import ColorLayer from "../assets/color Layer.png";
+import ColorLayer from "../assets/colorLayer.png";
 import { Play } from "lucide-react";
 
 const Banner = () => {
